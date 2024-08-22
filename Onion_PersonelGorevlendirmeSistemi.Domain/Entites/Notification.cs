3c6fd6c632f9fb616c1b19bdbbe019682455260c
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-08-22 11:56:00 +0300

    Notification entity için gerekli propertyler oluşturuldu

--- Onion_PersonelGorevlendirmeSistemi.Domain/Entites/Notification.cs
+++ Onion_PersonelGorevlendirmeSistemi.Domain/Entites/Notification.cs
@@ -8,5 +8,12 @@ namespace Onion_PersonelGorevlendirmeSistemi.Domain.Entites
 {
     public class Notification
     {
+        public int Id { get; set; }
+
+        public string Description { get; set; } = null!;//Açıklama boş geçilemez
+
+        public bool State { get; set; } //Okundu okunmadı anlamında
+
+        public int AppUserId { get; set; } //Hangi kullancıya gitmiş
     }
 }
