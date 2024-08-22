1dbc71322afef0ed356b45ae5bc8e6590bb58cf2
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-08-22 11:35:49 +0300

    PersonelGorevlendirme Sistemi İçin UI Katmanı oluşturuldu

---
+++ Onion_PersonelGorevlendirmeSistemi.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Onion_PersonelGorevlendirmeSistemi.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
