e3dee9a411ce4218d0bcf6c1af7eeeb06ad24535
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-08-24 17:10:11 +0300

    Application Katmanına Domain katmanı referans verildi

--- Onion_PersonelGorevlendirmeSistemi.Persistance/Context/TaskManagementContext.cs
+++ Onion_PersonelGorevlendirmeSistemi.Persistance/Context/TaskManagementContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace Onion_PersonelGorevlendirmeSistemi.Persistance.Context
 {
-    public class TaskManagementContext
+    public class TaskManagementContext:DbContext
     {
+        public TaskManagementContext(DbContextOptions<TaskManagementContext> options):base(options) //Base'ikinci overloadında parametre olarak gönderiyorum
+        {
+            
+        }
     }
 }
