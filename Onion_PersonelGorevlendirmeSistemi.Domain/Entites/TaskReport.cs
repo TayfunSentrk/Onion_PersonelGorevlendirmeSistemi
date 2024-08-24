fdd25febc8fdb66cc696926af322c5e57cf9d551
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-08-24 16:45:48 +0300

    TaskReport Entity için gerekli property'ler oluşturuldu

--- Onion_PersonelGorevlendirmeSistemi.Domain/Entites/TaskReport.cs
+++ Onion_PersonelGorevlendirmeSistemi.Domain/Entites/TaskReport.cs
@@ -8,5 +8,9 @@ namespace Onion_PersonelGorevlendirmeSistemi.Domain.Entites
 {
     public class TaskReport
     {
+        public int Id { get; set; }
+        public string Definition { get; set; } = null!;
+        public string Detail { get; set; } = null!;
+        public int AppTaskId { get; set; } //Hangi göreve ait olduğu
     }
 }
