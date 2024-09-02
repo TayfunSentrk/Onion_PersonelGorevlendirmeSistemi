766403b2f77dc9e04bd5d3f028a0a3182f5f8cbd
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-09-02 17:51:43 +0300

    AppRole entity için configrasyon işlemleri yapıldı

---
+++ Onion_PersonelGorevlendirmeSistemi.Persistance/Configurations/AppRoleConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Onion_PersonelGorevlendirmeSistemi.Domain.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onion_PersonelGorevlendirmeSistemi.Persistance.Configurations
+{
+    public class AppRoleConfiguration : IEntityTypeConfiguration<AppRole>
+    {
+        public void Configure(EntityTypeBuilder<AppRole> builder)
+        {
+            builder.Property(x => x.Definition).IsRequired(); //
+            builder.Property(x => x.Definition).HasMaxLength(150);
+            builder.HasMany(b => b.AppUsers).WithOne(appUser => appUser.AppRole).HasForeignKey(appUser => appUser.AppRoleId);
+        }
+    }
+}
